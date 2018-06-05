a35204ab8ff67157b59aab8c3a50d796732d9175
Genilson Reis <genilson.reis01@etec.sp.gov.br>
2018-06-05 16:47:15 -0300

    String

--- Strings/Exemplo_04.php
+++ Strings/Exemplo_04.php
@@ -4,14 +4,18 @@ $frase = "A repetição é a mãe da retenção.";
 
 $palavra = "mãe";
 
+//strpos — Encontra a posição da primeira ocorrência de uma string
 $q = strpos($frase, $palavra);
 
+//substr — Retorna uma parte de uma string
 $texto = substr($frase, 0, $q);
 
 var_dump($texto);
 
+//strlen — Retorna o tamanho de uma string
 $texto2 = substr($frase, $q + strlen($palavra), strlen($frase));
 
 echo "<br>";
 var_dump($texto2);
+
 ?>
